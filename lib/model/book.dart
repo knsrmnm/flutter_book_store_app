e69ccc77d7950a79668c3a8c276144edba22cff2
Ken Minami <kenminami.dev@gmail.com>
2022-02-21 14:00:11 +0900

    Add home screen and book model.

---
+++ lib/model/book.dart
@@ -0,0 +1,5 @@
+
+/// Book model
+class Book {
+
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Home Screen
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
